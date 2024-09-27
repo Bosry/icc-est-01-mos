9bec1161cd19c6ea591344cd31a62bf3ac535d9d
DomenikaD <ddelgadoi@est.ups.edu.ec>
2024-09-27 12:35:16 -0500

    Metodo de Seleccion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Metodo de Ordenamiento");
+        MetodosOrdenamiento mo = new MetodosOrdenamiento();
+        int[] arreglo = new int[] {10,5,8,2,0};
+        int[] arregloOr = mo.sortBySelection(arreglo);
+        mo.printArreglo(arregloOr);
     }
 }
